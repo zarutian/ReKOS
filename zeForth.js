6b153e6b26e592075b7bb9238dbb8c7a2c140868
Zarutian <zarutian@gmail.com>
2026-02-05 00:46:03 +0000

    Create zeForth.js
    Created file and started schlepping over stuff from my notes.

---
+++ zeForth.js
@@ -0,0 +1,30 @@
+const src = `
+
+  # looks like its save to start here in main storage
+  .org 0x2000
+  : __start
+  .dhw 0x1788        # XR  GR8, GR8              gr8 := 0
+  .dhw 0x4188 0x0200 # LA  GR8, 0x200 (GR8, 0)   gr8 := 0x200
+  .dhw 0x8980 0x0004 # SLL GR8, 0x004            gr8 := gr8 << 4
+  : NXT
+  .dhw 0x48A9 0x0000 # LH GR10, 0x000 (GR9, 0)   instr := memory[instr_ptr]
+  .dhw 0x4199 0x0002 # LA GR9,  0x002 (GR9, 0)   incr instr_ptr by halfcell (2 bytes)
+  .dhw 0x5410 0x82F6 # N  GR1,  0x2F6 (0, GR8)   tmp1 := tmp1 & 0xFFFF  cancel out the sign extension
+  .dhw 0x5810 0x82FC # L  GR1,  0x2FC (0, GR8)   tmp1 := 0xFFC0
+  .dhw 0x141A        # NR GR1,  GR10             tmp1 := tmp1 & instr
+  .dhw 0x4780 0x8030 # BC 8,    0x030 (0, GR8)   jump if result was zero
+  .dhw 0x509C 0x0000 # ST GR9,  0x000 (GR12, 0)  push instr_ptr onto returnstack
+  .dhw 0x41CC 0x0004 # LA GR12, 0x004 (GR12, 0)  incr returnstack_ptr by 4
+  .dhw 0x1799        # XR GR9,  GR9              zero out instr_ptr ...
+  .dhw 0x179A        # XR GR9,  GR10             instr_ptr := instr
+  .dhw 0x47F0 0x8000 # BC 0xF,  0x000 (0, GR8)   jump to NXT
+  .dhw 0x1711        # XR GR1,  GR1              zero out tmp1
+  .dhw 0x171A        # XR GR1,  GR10             tmp1 := instr
+  .dhw 0x8910 0x0001 # SLL GR1, 1 (0, 0)         tmp1 := tmp1 << 1
+  .dhw 0x4821 0x8302 # LH GR2,  0x302 (GR1, GR8) tmp2 := memory[tmp1 + ibm370_start + opcode_jmptbl]
+  .dhw 0x5420 0x82F6 # N  GR2,  0x2F6 (0, GR8)   tmp2 := tmp2 & 0xFFFF   cancel out the sign extension
+  .dhw 0x07F2        # BCR 0xF, GR2
+
+`
+const img = new Map();
+assemble({ src, img });
