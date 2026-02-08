a74868a495e11e90992a098563f2f25be72061f8
Zarutian <zarutian@gmail.com>
2026-02-08 14:54:48 +0000

    Create IBM_z390_consts.js

---
+++ IBM_z390_consts.js
@@ -0,0 +1,23 @@
+
+const src = `
+  # Various IBM z/Arch and ESA/390 constants and locations
+
+  # z/Arch
+  .def IBMz__IPL_PSW_addr     0x0000
+  .def IBMz__IPL_CCW1_addr    0x0008
+  .def IBMz__IPL_CCW2_addr    0x0010
+
+  .def IBMz__External_Interruption_parameter_addr  0x0080
+  .def IBMz__External_Interruption_CPU_ADDR_addr   0x0084
+  .def IBMz__External_Interruption_Code_addr       0x0086
+
+  .def IBMz__Supervisor_CallyInterruption_Identification_addr  0x0088
+  .def IBMz__Program_InterruptionyIdentification_addr          0x008C
+  
+  .def IBMz__Data_Exception_Code_addr              0x0090
+  .def IBMz__Monitor_Class_Number_addr             0x0094
+  .def IBMz__PER_Code_addr                         0x0096
+  .def IBMz__PER_Address_addr                      0x0098
+
+`;
+export { src }
