760ff51cb389a6cea0fac1ae96dc1db98f8f2565
Zarutian <zarutian@gmail.com>
2026-04-01 13:22:29 +0000

    .

--- src/zeForth_tcl.js
+++ src/zeForth_tcl.js
@@ -1,4 +1,30 @@
 const src = `
+  # a bit unrelated but wanted to save this from a handwritten note.
+
+  :f 3NIP
+  # ( a b c -- c )
+  .dhw >R 3DROP R> EXIT
+
+  :f 0<=
+  # ( num -- flag )
+  .dhw DUP 0< SWAP 0= OR EXIT
+
+  :f 2RDROP
+  # ( -- ) R:( a b raddr -- raddr )
+  .dhw R> RDROP RDROP >R EXIT
+
+  :f 4DUP
+  # ( a b c d -- a b c d a b c d )
+  .dhw >R >R               # ( a b ) R:( d c )
+  .dhw 2DUP                # ( a b a b ) R:( d c )
+  .dhw R@ -ROT             # ( a b c a b ) R:( d c )
+  .dhw R> R> SWAP >R DUP   # ( a b c a b d d ) R:( c )
+  .dhw >R -ROT             # ( a b c d a b ) R:( c d )
+  .dhw R> R> SWAP EXIT     # ( a b c d a b c d ) R:( )
+
+  :f 4DROP
+  # ( a b c d -- )
+  .dhw 2DROP 2DROP EXIT
 
 `;
 export { src };
