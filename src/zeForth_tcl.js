5e7cdb2a57427de4597f51bc14c28b61d5e6155d
Zarutian <zarutian@gmail.com>
2026-04-01 13:24:03 +0000

    .

--- src/zeForth_tcl.js
+++ src/zeForth_tcl.js
@@ -26,5 +26,27 @@ const src = `
   # ( a b c d -- )
   .dhw 2DROP 2DROP EXIT
 
+  :f QROT
+  # ( a b c d -- b c d a )
+  .dhw >R ROT R> SWAP EXIT
+
+  :f -QROT
+  # ( a b c d -- d a b c )
+  .dhw -ROT  # ( a d b c )
+  .dhw >R >R # ( a d ) R:( c b )
+  .dhw SWAP  # ( d a ) R:( c b )
+  .dhw R> R> # ( d a b c ) R:( )
+  .dhw EXIT
+
+  :f Tcl_list_first
+  # ( addr len -- addr len' )
+  # Takes an ASCII string and returns the first 'word' of it per Tcl parsing rules
+  # todo eventually: make an utf-8 version
+  .dhw SWAP TUCK           # ( addr addr len )
+  .dhw Tcl_list_first_sub0 # ( addr bralvl brclvl inquote addr' )
+  .dhw 2NIP                # ( addr addr' )
+  .dhw OVER MINUS          # ( addr len' )
+  .dhw EXIT
+
 `;
 export { src };
