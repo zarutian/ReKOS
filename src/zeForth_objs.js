19af308908cc926c89bc1fade304e089fe034392
Zarutian <zarutian@gmail.com>
2026-05-17 20:39:47 +0000

    Update zeForth_objs.js

--- src/zeForth_objs.js
+++ src/zeForth_objs.js
@@ -32,7 +32,7 @@ const src = `
   .dhw EXIT
   
   : zobj_ptr-
-  # ( ptr offsett -- ptr )
+  # ( ptr offset -- ptr )
   .dhw >R
   .dhw zobj_objspace_start
   .dhw -
@@ -44,6 +44,45 @@ const src = `
   .dhw +
   .dhw EXIT
 
+  : zobj_ptr++
+  # ( ptr -- ptr+1 )
+  .dhw LIT_1
+  .dhw zobj_ptr+
+  .dhw EXIT
+
+  # obj header: 0b ttzz zzSS SSSS SSSS
+  #   t: type
+  #   z: size of refs size in S
+  #   S: datum size in words | refs size in refs
+  : zobj_header@
+  # ( optr -- hdr )
+  .dhw zobj_@
+  .dhw EXIT
+  
+  : zobj_header!
+  .dhw zobj_!
+  .dhw EXIT
+  
+  : zobj_invocation_handler@
+  # ( optr -- xt )
+  : zobj_xt@
+  # ( optr -- xt )
+  .dhw zobj_ptr++
+  .dhw zobj_@
+  .dhw EXIT
+  
+  : zobj_invocation_handler!
+  # ( xt optr -- )
+  : zobj_xt!
+  # ( xt optr -- )
+  .dhw zobj_ptr++
+  .dhw zobj_!
+  .dhw EXIT
+  
+  : zobj_sizeof_ref
+  .dhw (CONST)
+  .dhw 1
+  
 `;
 export { src };
 
